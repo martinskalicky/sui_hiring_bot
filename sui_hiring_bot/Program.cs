44124ca58836e3ae6d8052fbfb7317fd5b33b272
Martin Skalický <martin.skalicky@cimpress.com>
2021-08-13 18:46:27 +0200

    init

---
+++ sui_hiring_bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sui_hiring_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
