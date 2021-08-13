5803e9a8b7e112ca3120dee0bd70276057b81252
Martin Skalický <martin.skalicky@cimpress.com>
2021-08-13 19:11:34 +0200

    Adding discord layer

--- sui_hiring_bot/Program.cs
+++ sui_hiring_bot/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace sui_hiring_bot
 {
     class Program
     {
+        private DiscordSocketClient _client;
+        private ulong _channelId;
+        private string? _token;
+
         static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            _channelId = Convert.ToUInt64(Environment.GetEnvironmentVariable("CHANNEL_ID"));
+            _client.MessageReceived += MessageReceived;
+
+            _token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (_client.GetChannel(_channelId) == message.Channel && message.Content.Contains("!test"))
+            {
+                await message.Channel.SendMessageAsync("I'm working!");
+            }
+            if (_client.GetChannel(_channelId) == message.Channel && message.Content.Equals("!exit"))
+            {
+                ExitApplication();
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        
+        private void ExitApplication()
+        {
+            Environment.Exit(0);
         }
     }
 }
