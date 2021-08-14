17cf68383fcda1dea80220dee8d1cd627b91239a
Luigi Banzato <banzato.luigi@gmail.com>
2021-08-14 03:05:02 +0100

    adding a basic call to zkillboard for character scanning/testing

---
+++ sui_hiring_bot/Character.cs
@@ -0,0 +1,8 @@
+namespace sui_hiring_bot
+{
+    class Character
+    {
+        public string id { get; set; }
+
+    }
+}
--- sui_hiring_bot/Program.cs
+++ sui_hiring_bot/Program.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace sui_hiring_bot
 {
@@ -10,6 +12,7 @@ namespace sui_hiring_bot
         private DiscordSocketClient _client;
         private ulong _channelId;
         private string? _token;
+        private HttpClient httpClient;
 
         static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
@@ -22,13 +25,46 @@ namespace sui_hiring_bot
             _client.MessageReceived += MessageReceived;
 
             _token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
+            Console.WriteLine("_token = " + _token);
 
             await _client.LoginAsync(TokenType.Bot, _token);
             await _client.StartAsync();
+
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://zkillboard.com/autocomplete/");
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; AcmeInc/1.0)");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
             await Task.Delay(-1);
         }
         private async Task MessageReceived(SocketMessage message)
         {
+            if (_client.GetChannel(_channelId) == message.Channel && message.Content.Contains("!character"))
+            {
+                String characterName = message.Content.Substring(11);
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(characterName);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        String resp = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(resp);
+                        await message.Channel.SendMessageAsync(resp);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error! Response:");
+                        Console.WriteLine(response);
+                        await message.Channel.SendMessageAsync("Error obtaining data from zkillboard for a character with the name '" + characterName + "'");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
             if (_client.GetChannel(_channelId) == message.Channel && message.Content.Contains("!test"))
             {
                 await message.Channel.SendMessageAsync("I'm working!");
