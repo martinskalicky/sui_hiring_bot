63c2a86551bf25ff6838f4cb564792dc926d62e5
Lomion-tm <d4nybernard@gmail.com>
2021-08-19 18:19:10 +0200

    get Access Token
    Client ID:Secret Key, refresh token & sender id have to be provided.
    Access Token gets fetched before sending mail.

--- sui_hiring_bot/SendMailToChar.cs
+++ sui_hiring_bot/SendMailToChar.cs
@@ -14,8 +14,26 @@ namespace sui_hiring_bot
         {
             try
             {
+                httpClient_token = new HttpClient();
+                httpClient_token.DefaultRequestHeaders.Accept.Clear();
+                //App Client ID:Secret Key from https://developers.eveonline.com
+                var byteArray = System.Text.Encoding.ASCII.GetBytes("Client ID:Secret Key");
+                httpClient_token.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                var values = new System.Collections.Generic.Dictionary<string, string>
+                {
+                    { "grant_type", "refresh_token" },
+                    { "refresh_token", "XXXXXXXXXXXXXXXX" } //REFRESH TOKEN OF SENDER
+                };
+                string url = "https://login.eveonline.com/v2/oauth/token";
+                var data = new FormUrlEncodedContent(values);
+                var res = await httpClient_token.PostAsync(url,data);
+                string responseBody = await res.Content.ReadAsStringAsync();
+                dynamic stuff = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+                
                 httpClient = new HttpClient();
                 httpClient.DefaultRequestHeaders.Accept.Clear();
+                var characterIdSender = "XXXXXX"; //SENDER ID
+                var token = stuff.access_token.ToString();
 
                 var json = "{\"approved_cost\": 0,\"body\": \"" + emailPayload + "\",\"recipients\": [{\"recipient_id\": " + characterId + ",\"recipient_type\": \"character\"}],\"subject\": \"Its alive!!\"}";
 
